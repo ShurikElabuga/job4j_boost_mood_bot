622d6710a20beb8ef9258bde6ae227263cbecac6
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-01-24 00:34:35 +0300

    Механизм рассылки уведомлений [#505297]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,23 +2,24 @@ package ru.job4j;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import ru.job4j.bmb.content.Content;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.job4j.bmb.services.TelegramBotService;
 
+@EnableScheduling
+@SpringBootApplication
 public class Main {
-    Long aLong = 64363636363636L;
 
     public static void main(String[] args) {
-        SpringApplication.run(ru.job4j.bmb.Main.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     @Bean
     public CommandLineRunner initTelegramApi(ApplicationContext ctx) {
         return args -> {
             var bot = ctx.getBean(TelegramBotService.class);
-            bot.receive(new Content(aLong));
         };
     }
 }
