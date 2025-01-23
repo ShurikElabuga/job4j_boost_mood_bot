f1cb5170ae7d9ad0ba29a7b73b770a7fb96ff976
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-01-23 22:52:34 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/recommendations/RecommendationEngine.java
+++ src/main/java/ru/job4j/recommendations/RecommendationEngine.java
@@ -1,12 +1,28 @@
 package ru.job4j.recommendations;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.List;
+import java.util.Random;
 
-@Component
+@Service
 public class RecommendationEngine {
+
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
+    }
+
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
+    }
 
     @PostConstruct
     public void init() {
