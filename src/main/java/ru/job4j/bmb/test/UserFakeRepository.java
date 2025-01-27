41f9feadbb1864421191a3a0e44fd1e1298a9055
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-01-27 22:11:56 +0300

    Механизм рассылки уведомлений [#505297 #529836]

--- src/main/java/ru/job4j/bmb/test/UserFakeRepository.java
+++ src/main/java/ru/job4j/bmb/test/UserFakeRepository.java
@@ -1,8 +1,9 @@
-package ru.job4j.bmb.repository;
+package ru.job4j.bmb.test;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
