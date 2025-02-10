b6e19103f6a2c405c1759da1fa004a07b6d45cac
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-02-10 22:18:57 +0300

    MoodService - бизнес логика системы [#505299 #530957]

--- src/main/java/ru/job4j/bmb/repository/MoodLogRepository.java
+++ src/main/java/ru/job4j/bmb/repository/MoodLogRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import ru.job4j.bmb.model.MoodLog;
 import java.util.List;
 
-
 public interface MoodLogRepository extends CrudRepository<MoodLog, Long> {
 
     List<MoodLog> findAll();
