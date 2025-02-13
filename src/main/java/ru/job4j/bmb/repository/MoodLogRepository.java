b9d28626ac97cd4f85a180a76bbdf8927b27853b
ShurikElabuga <ShurikElabuga@yandex.ru>
2025-02-13 21:41:35 +0300

    ApplicationListener | AchievementService [#505294 #531870]

--- src/main/java/ru/job4j/bmb/repository/MoodLogRepository.java
+++ src/main/java/ru/job4j/bmb/repository/MoodLogRepository.java
@@ -2,12 +2,14 @@ package ru.job4j.bmb.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.bmb.model.MoodLog;
+import ru.job4j.bmb.model.User;
+
 import java.util.List;
 
 public interface MoodLogRepository extends CrudRepository<MoodLog, Long> {
 
     List<MoodLog> findAll();
 
-    List<MoodLog> findByUserId(Long userId);
+    List<User> findUsersWhoDidNotVoteToday(long startOfDay, long endOfDay);
 
 }
--- src/test/java/ru/job4j/bmb/repository/MoodFakeRepository.java
+++ src/test/java/ru/job4j/bmb/repository/MoodFakeRepository.java
@@ -2,9 +2,9 @@ package ru.job4j.bmb.repository;
 
 import org.springframework.test.fake.CrudRepositoryFake;
 import ru.job4j.bmb.model.Mood;
-import ru.job4j.bmb.repository.MoodRepository;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoodFakeRepository extends CrudRepositoryFake<Mood, Long> implements MoodRepository {
@@ -12,5 +12,16 @@ public class MoodFakeRepository extends CrudRepositoryFake<Mood, Long> implement
     @Override
     public List<Mood> findAll() {
         return new ArrayList<>(memory.values());
+    }
+
+    @Override
+    public Mood save(Mood mood) {
+        if (mood.getId() == null) {
+            Long lastId = memory.keySet().stream()
+                            .max(Comparator.naturalOrder())
+                                    .orElse(0L);
+            mood.setId(lastId + 1);
+        }
+        return super.save(mood);
     }
 }
